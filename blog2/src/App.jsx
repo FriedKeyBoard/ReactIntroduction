32c9a3739202d8f299a72d796562fb45ff3f2a03
FriedKeyBoard <koteha@naver.com>
2025-08-27 22:08:55 +0900

    map을 이용한 반복문

--- blog2/src/App.jsx
+++ blog2/src/App.jsx
@@ -5,16 +5,23 @@ import './App.css'
 
 function App() {
     let [title,setTitle] = useState(['남자 코트 추천', '남자 바지 추천', '연희동 맛집 추천']);
-    let [likeCount, setLikeCount] = useState(0);
+    let [likeCount, setLikeCount] = useState([0,0,0]);
     let [modal, setModal] = useState(false);
 
-    let posts = '고기 맛집'
+
     function titleMod() {
         var newArr = [...title];
         newArr.sort();
 
         setTitle(newArr);
     }
+
+    function addLikeCount(i){
+        var newlikeCount = {...likeCount}
+        newlikeCount[i] = newlikeCount[i]+1;
+        setLikeCount(newlikeCount);
+    }
+
   return (
       <div>
           <div className="black-nav">
@@ -22,21 +29,33 @@ function App() {
               {/*<div className={posts} style={{color : 'blue', fontSize : '30px'}}>개발 Blog</div>*/}
           </div>
           <button onClick={titleMod}>button</button>
-          <div className="list">
-              <h3> {title[0]} <span onClick={ ()=>setLikeCount(likeCount+1)}>👍</span> {likeCount} </h3>
-              <p>2월 17일 발행</p>
-              <hr/>
-          </div>
-          <div className="list">
-              <h3> {title[1]} </h3>
-              <p>2월 17일 발행</p>
-              <hr/>
-          </div>
-          <div className="list">
-              <h3 onClick={ () => modal === false ? setModal(true) : setModal(false)}> {title[2]} </h3>
-              <p>2월 17일 발행</p>
-              <hr/>
-          </div>
+          {/*<div className="list">*/}
+          {/*    <h3> {title[0]} <span onClick={ ()=>setLikeCount(likeCount+1)}>👍</span> {likeCount} </h3>*/}
+          {/*    <p>2월 17일 발행</p>*/}
+          {/*    <hr/>*/}
+          {/*</div>*/}
+          {/*<div className="list">*/}
+          {/*    <h3> {title[1]} </h3>*/}
+          {/*    <p>2월 17일 발행</p>*/}
+          {/*    <hr/>*/}
+          {/*</div>*/}
+          {/*<div className="list">*/}
+          {/*    <h3 onClick={ () => modal === false ? setModal(true) : setModal(false)}> {title[2]} </h3>*/}
+          {/*    <p>2월 17일 발행</p>*/}
+          {/*    <hr/>*/}
+          {/*</div>*/}
+
+          {
+              title.map(function (a,i){
+                  return (
+                      <div className="list" key={i}>
+                          <h3 onClick={ () => setModal(!modal)}> {title[i]} <span onClick={() => addLikeCount(i)}>👍</span> {likeCount[i]} </h3>
+                          <p>2월 17일 발행</p>
+                          <hr/>
+                      </div>
+                  )
+              })
+          }
 
           {
             modal === true ? <Modal></Modal> : null
